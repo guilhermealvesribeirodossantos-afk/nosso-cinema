16fff809767db307386071ea1ccb2b4f0b2ec92a
guilhermealvesribeirodossantos-afk <guilhermealvesribeirodossantos@gmail.com>
2026-09-13 08:33:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const enterButton = document.getElementById("enterButton");
 
+const hero = document.querySelector(".hero");
+const home = document.getElementById("home");
+
+enterButton.addEventListener("click", () => {
+
+    hero.style.display = "none";
+
+    home.classList.remove("hidden");
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
